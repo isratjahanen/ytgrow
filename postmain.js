65135571320916617f760ed1903e54e67888a5aa
isratjahanen <80692986+isratjahanen@users.noreply.github.com>
2021-07-21 19:42:03 +0600

    Update postmain.js

--- postmain.js
+++ postmain.js
@@ -196,3 +196,113 @@ angular.module('myapps').filter('pagination', function(){
 
  
 	
+
+function sowurl(){
+  var checkurlresult = document.getElementById("checkurlresult").value;
+    if(checkurlresult=="show"){
+    
+    document.getElementById("urlshow").style.display = "block";
+    }
+    
+    else{
+    document.getElementById("urlhide").style.display = "block";
+    }
+}
+
+$( document ).ready(function() {
+
+    var opcionesnav = $('.navoption').length;
+    var clickhamb=0;
+
+    $("#hamburger").click(function(){
+        clickhamb = 1;
+        var header = $("#ap");
+        if (header[0].classList.length == 1) {
+            header.addClass ("responsive");
+            $("header").height((opcionesnav+1)*48);
+            $(".navlist a:not(.icon)").css("display", "block");
+            setTimeout(
+                function()
+                {
+                    $(".navlist a:not(.icon)").css("transform", "translateX(0px)");
+                }, 50);
+
+        } else {
+            $(".navlist a:not(.icon)").css("transform", "translateX(600px)");
+            header.height(75);
+            setTimeout(
+                function()
+                {
+                    header.removeClass("responsive");
+                    header.height(75);
+                    $(".navlist a:not(.icon)").css("display", "none");
+                }, 1600);
+        }
+    });
+
+
+    $(window).on('resize', function(){
+        console.log(clickhamb);
+        if (($(window).width() > 600) && (clickhamb==1)){
+            console.log(clickhamb + "     " + $(window).width());
+            $("#ap").height(75);
+            $(".navlist a:not(.icon)").css("display", "block");
+            setTimeout(
+                function()
+                {
+                    $(".navlist a:not(.icon)").css("transform", "translateX(0px)");
+                }, 500);
+        }
+    });
+
+});
+
+
+$( document ).ready(function() {
+
+    var opcionesnav = $('.navoptions').length;
+    var clickhamb=0;
+
+    $("#hamburgers").click(function(){
+        clickhamb = 1;
+        var header = $("#pr");
+        if (header[0].classList.length == 1) {
+            header.addClass ("responsive");
+            $("header").height((opcionesnav+1)*48);
+            $(".navlist a:not(.icon)").css("display", "block");
+            setTimeout(
+                function()
+                {
+                    $(".navlist a:not(.icon)").css("transform", "translateX(0px)");
+                }, 50);
+
+        } else {
+            $(".navlist a:not(.icon)").css("transform", "translateX(600px)");
+            header.height(75);
+            setTimeout(
+                function()
+                {
+                    header.removeClass("responsive");
+                    header.height(75);
+                    $(".navlist a:not(.icon)").css("display", "none");
+                }, 1600);
+        }
+    });
+
+
+    $(window).on('resize', function(){
+        console.log(clickhamb);
+        if (($(window).width() > 600) && (clickhamb==1)){
+            console.log(clickhamb + "     " + $(window).width());
+            $("#pr").height(75);
+            $(".navlist a:not(.icon)").css("display", "block");
+            setTimeout(
+                function()
+                {
+                    $(".navlist a:not(.icon)").css("transform", "translateX(0px)");
+                }, 500);
+        }
+    });
+
+});
+
